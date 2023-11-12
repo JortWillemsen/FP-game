17dc1dd823889c082e5d44c84854045af04ef655
Annemae <annemae.vandehoef@gmail.com>
2023-11-12 16:48:42 +0100

    added last level

---
+++ src/View.hs
@@ -0,0 +1,21 @@
+-- | This module defines how to turn
+--   the game state into a picture
+module View where
+
+import Graphics.Gloss
+import Model
+import Player
+import Ghost (Ghost(..))
+
+view :: GameState -> IO Picture
+view = return . showAll 
+ 
+showAll gstate = Pictures [showPlayer gstate, showGhost gstate]
+
+showPlayer gstate = case player gstate of 
+    (PuckMan (x, y) _) -> translate x y (color yellow (circle 30))
+
+showGhost gstate = case blinky gstate of 
+    (Blinky (x, y)) -> translate x y (color green (circle 20))
+
+    
